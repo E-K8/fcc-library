218e56fa0e0915b9f07e615dbfdd80beba1fda7b
E-K8 <ekatplat@gmail.com>
2024-07-06 20:34:42 +0100

    add connection to MongoDB, add POST request to api/books

--- db-connection.js
+++ db-connection.js
@@ -2,11 +2,6 @@ const mongoose = require('mongoose');
 
 const uri = process.env.DB;
 
-if (!uri) {
-  console.error('Error: The DB environment variable is not set.');
-  process.exit(1); // Exit the process with an error code
-}
-
 const db = mongoose.connect(uri);
 
 module.exports = db;
